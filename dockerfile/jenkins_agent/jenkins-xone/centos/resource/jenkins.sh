d0824edff3f6fd335fe2800da115367dd867757b
ops <ops@xuncetech.com>
2019-05-23 14:23:43 +0800

    agent

--- dockerfile/jenkins_agent/jenkins-xone/centos/resource/jenkins.sh
+++ dockerfile/jenkins_agent/jenkins-xone/centos/resource/jenkins.sh
@@ -4,4 +4,4 @@ if [ -d ${JENKINS_AGENT_WORKDIR}/build ]; then
     find ${JENKINS_AGENT_WORKDIR}/build -name "*.sh" -o -name "*.py"|xargs -I {} chmod +x {}
     chown -R ${JENKINS_USER:-jenkins}. ${JENKINS_AGENT_WORKDIR}/build
 fi
-su - ${JENKINS_USER:-jenkins} -c "java -jar /usr/bin/agent.jar -jnlpUrl ${JENKINS_URL} -secret ${JENKINS_SECRET} -workDir ${JENKINS_AGENT_WORKDIR}"
+su - ${JENKINS_USER:-jenkins} -c "cd ${JENKINS_AGENT_WORKDIR};java -jar /usr/bin/agent.jar -jnlpUrl ${JENKINS_URL} -secret ${JENKINS_SECRET} -workDir ${JENKINS_AGENT_WORKDIR}"
