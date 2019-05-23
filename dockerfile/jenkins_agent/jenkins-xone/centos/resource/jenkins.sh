595200cc71888007eaa4e5a493a3eef4780c5916
qiaodapei <371583076@qq.com>
2019-05-23 14:46:04 +0800

    agent

--- dockerfile/jenkins_agent/jenkins-xone/centos/resource/jenkins.sh
+++ dockerfile/jenkins_agent/jenkins-xone/centos/resource/jenkins.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if [ -d ${JENKINS_AGENT_WORKDIR}/build ]; then
-    find ${JENKINS_AGENT_WORKDIR}/build -name "*.sh" -o -name "*.py"|xargs -I {} chmod +x {}
-    chown -R ${JENKINS_USER:-jenkins}. ${JENKINS_AGENT_WORKDIR}/build
+if [ -d ${JENKINS_AGENT_WORKDIR} ]; then
+    find ${JENKINS_AGENT_WORKDIR} -name "*.sh" -o -name "*.py"|xargs -I {} chmod +x {}
+    chown -R ${JENKINS_USER:-jenkins}. ${JENKINS_AGENT_WORKDIR}
 fi
 su - ${JENKINS_USER:-jenkins} -c "java -jar /usr/bin/agent.jar -jnlpUrl ${JENKINS_URL} -secret ${JENKINS_SECRET} -workDir ${JENKINS_AGENT_WORKDIR}"
