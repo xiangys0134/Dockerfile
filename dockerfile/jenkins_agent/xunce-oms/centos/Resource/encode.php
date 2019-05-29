0ef42581546c32695145e8fc0bdfb2826d16015a
Your Name <you@example.com>
2019-05-29 08:50:42 +0800

    aaa

---
+++ dockerfile/jenkins_agent/xunce-oms/centos/Resource/encode.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Usage:
+ * php encode_file.php --oldfile old_file_path --newfile new_file_path --encrypt DES --expire "2016-10-10 10:10:10"
+ * @author: liexusong
+ */
+$oldfile = $argv[1];
+$newfile = $argv[2];
+$datetime = '+1 year';
+$encrypt = 'DES';
+switch ($encrypt) {
+    case 'DES':
+        $type = BEAST_ENCRYPT_TYPE_DES;
+        break;
+    case 'AES':
+        $type = BEAST_ENCRYPT_TYPE_AES;
+        break;
+    case 'BASE64':
+        $type = BEAST_ENCRYPT_TYPE_BASE64;
+        break;
+    default:
+        $type = BEAST_ENCRYPT_TYPE_DES;
+        break;
+}
+
+if (empty($oldfile) || !file_exists($oldfile)) {
+    echo "Encrypt file `{$oldfile}' not found!\n";
+    exit(1);
+}
+
+// if (empty($newfile)) {
+//     $paths = explode('.', basename($oldfile));
+//     $exten = $paths[count($paths) - 1];
+//     unset($paths[count($paths) - 1]);
+//     $name = implode('.', $paths);
+//     $newfile = dirname($oldfile) . '/' . $name . '_enc.' . $exten;
+// }
+// echo "Starting encrypt `{$oldfile}' and save to `{$newfile}'\n";
+// echo "Expire time: {$datetime}, using encrypt type: $encrypt\n";
+$expire = $datetime ? strtotime($datetime) : 0;
+if (beast_encode_file($oldfile, $newfile, $expire, $type)) {
+    echo "Encrypt file success!\n";
+} else {
+    echo "Encrypt file failure!\n";
+}
