b525458feca780bd6e7a552144cd713422e5236f
Your Name <you@example.com>
2019-08-22 22:59:01 +0800

    docker-git

--- dockerfile/jenkins_agent/jenkins-docker/resource/jenkins.sh
+++ dockerfile/jenkins_agent/jenkins-docker/resource/jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-java -jar /usr/bin/agent.jar -jnlpUrl ${JENKINS_URL} -secret ${JENKINS_SECRET} -workDir ${JENKINS_AGENT_WORKDIR}
+java -jar /usr/bin/agent.jar -jnlpUrl ${JENKINS_URL} -secret ${JENKINS_SECRET} -workDir ${JENKINS_AGENT_WORKDIR} && sh /usr/local/bin/docker-entrypoint.sh
